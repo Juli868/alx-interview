d76ae1d9b9acc3b476210ca879b8561962406d2c
juli868 <bugijul@2001>
2024-05-17 10:52:02 +0200

    Adittional on the conditions.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Calculating a perimeter of a grid."""
+
+
 def island_perimeter(grid):
     """Return the perimeter."""
     result = 0
@@ -7,8 +9,8 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 result += 4
-                if grid[i][j-1] == 1:
+                if j > 0 and grid[i][j-1] == 1:
                     result -= 2
-                if grid[i-1][j] == 1:
+                if i > 0 and grid[i-1][j] == 1:
                     result -= 2
     return result
